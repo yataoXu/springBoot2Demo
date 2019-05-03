b5d98ad76db7fbbc58e87f01a71096634abd6973
Evan <xuyt1992@163.com>
2019-05-03 23:46:21 +0800

    多数据源

---
+++ springboot-multi-jdbctemplate/src/main/java/com/yatao/multi/jdbctemplate/SpringbootMultiJdbctemplateApplication.java
@@ -0,0 +1,13 @@
+package com.yatao.multi.jdbctemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMultiJdbctemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMultiJdbctemplateApplication.class, args);
+    }
+
+}
